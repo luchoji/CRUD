579ae272544974d9d957dafddfc24df82f7fbf1b
Dylan2256521 <dilanmachado89@gmail.com>
2024-10-24 02:04:24 -0500

    subir

--- eliminar.php
+++ eliminar.php
@@ -1,4 +1,6 @@
 <?php
+session_start(); // Iniciar la sesión
+
 // Incluir el archivo de conexión a la base de datos
 include 'conexion.php';
 
@@ -12,8 +14,11 @@ if (isset($_GET['id'])) {
         mysqli_stmt_bind_param($stmt, "i", $id);
 
         if (mysqli_stmt_execute($stmt)) {
+            // Si la eliminación es exitosa, establecer un mensaje en la sesión
+            $_SESSION['mensaje'] = "Eliminación exitosa";
+            // Redirigir al usuario a la página principal (index.php)
             header("Location: index.php");
-            exit();
+            exit(); // Terminar el script para evitar la ejecución de código innecesario
         } else {
             echo "Error al eliminar el registro: " . mysqli_error($conn);
         }
@@ -25,3 +30,4 @@ if (isset($_GET['id'])) {
 // Cerrar la conexión a la base de datos
 mysqli_close($conn);
 ?>
+
